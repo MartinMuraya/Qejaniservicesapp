2d15bb69f61de45350e57f45c14b36a62d5b251a
MartinMuraya <gathongomoses14@gmail.com>
2025-12-17 19:56:06 +0300

    backend

--- routes/mpesaRoutes.js
+++ routes/mpesaRoutes.js
@@ -1,8 +1,11 @@
 import express from "express";
-import { stkPush, mpesaCallbackFix } from "../controllers/mpesaController.js";
+import { stkPush, mpesaCallbackFix, finishPayment } from "../controllers/mpesaController.js";
+import { protect } from "../middlewares/authMiddleware.js";
+
 const router = express.Router();
 
 router.post("/stkpush", stkPush);
 router.post("/callback", mpesaCallbackFix);
+router.post("/finish-payment", protect, finishPayment);
 
 export default router;
--- server.js
+++ server.js
@@ -23,14 +23,17 @@ const app = express();
 const server = http.createServer(app);
 export const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173', // frontend URL
+    origin: ["http://localhost:5173", "http://localhost:5174"], // frontend URL
     methods: ['GET','POST','PUT','DELETE']
   }
 });
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174"
+  ],       
   methods: ['GET','POST','PUT','DELETE'],
   credentials: true
 }));
